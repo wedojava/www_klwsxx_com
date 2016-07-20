34e88cfef3238b0788cedf6a5efa7079acbc348f
回风 <wedojava@bhl.me>
2016-07-20 10:47:12 +0800

    make ico and add object.

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="assets/bootstrap-3.3.5-dist/css/bootstrap.min.css" tppabs="http://v3.bootcss.com/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/font-awesome-4.6/css/font-awesome.min.css" tppabs="http://v3.bootcss.com/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js" tppabs="http://v3.bootcss.com/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -50,8 +51,8 @@
               <ul class="nav navbar-nav">
                 <li class="active"><a href="#">首页</a></li>
                 <li><a href="#about">业务范围</a></li>
-                <li><a href="#contact">业务发展</a></li>
-                <li class="dropdown">
+                <li><a href="#contact">联系我们</a></li>
+                <!-- <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
                   <ul class="dropdown-menu" role="menu">
                     <li><a href="#">Action</a></li>
@@ -62,7 +63,7 @@
                     <li><a href="#">Separated link</a></li>
                     <li><a href="#">One more separated link</a></li>
                   </ul>
-                </li>
+                </li> -->
               </ul>
             </div>
           </div>
@@ -97,7 +98,7 @@
           <img src="data:image/gif;base64,R0lGODlhAQABAIAAAGZmZgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
           <div class="container">
             <div class="carousel-caption">
-              <h1>Good Job!</h1>
+              <h1>军民融合 特色发展</h1>
               <p>公司拥有一支富有实战经验的博士、硕士、高中级工程师等组成的软硬件开发团队，在军民融合国家战略的大背景下，以军人坚韧不拔的意志品质，坚持创新发展理念，凭借雄厚的技术实力，专注于网络安全、数据分析、专用通信等前沿科技领域的软硬件开发和系统解决方案。</p>
               <p><a class="btn btn-lg btn-primary" href="#" role="button">了解更多</a></p>
             </div>
@@ -107,7 +108,7 @@
           <img src="data:image/gif;base64,R0lGODlhAQABAIAAAFVVVQAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
           <div class="container">
             <div class="carousel-caption">
-              <h1>On The Way!</h1>
+              <h1>产品与服务领先提供商</h1>
               <p>公司成立以来先后开发了10余个有自主知识产权的科技项目，已与乌鲁木齐红山一卡通公司签订了战略合作协议，先后参与了自治区信访局、新疆农信社、国网新疆电力公司信息通信公司等单位的软硬件开发工作，在疆内走出了一条军民融合的特色发展路子。</p>
               <p><a class="btn btn-lg btn-primary" href="#" role="button">了解更多</a></p>
             </div>
@@ -130,78 +131,73 @@
     ================================================== -->
     <!-- Wrap the rest of the page in another container to center all the content. -->
 
-    <div class="container marketing">
+    <div id="about" class="container marketing">
 
       <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+        <span class="fa-stack fa-5x text-warning" aria-hidden="true">
+          <i class="fa fa-circle fa-stack-2x "></i>
+          <i class="fa fa-shield fa-stack-1x fa-inverse "></i>
+        </span>
           <h2>网络安全</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+          <!-- <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p> -->
+          <p><a class="btn btn-default" href="#" role="button">了解更多 <i class="fa fa-arrow-circle-right"></i> <!-- » --></a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+        <span class="fa-stack fa-5x text-success" aria-hidden="true">
+          <i class="fa fa-circle fa-stack-2x "></i>
+          <i class="fa fa-database fa-stack-1x fa-inverse "></i>
+        </span>
           <h2>数据分析</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+          <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
+          <p><a class="btn btn-default" href="#" role="button">了解更多 <i class="fa fa-arrow-circle-right"></i> <!-- » --></a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+        <span class="fa-stack fa-5x text-primary" aria-hidden="true">
+          <i class="fa fa-circle fa-stack-2x "></i>
+          <i class="fa fa-fax fa-stack-1x fa-inverse "></i>
+        </span>
           <h2>专用通信</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+          <!-- <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p> -->
+          <p><a class="btn btn-default" href="#" role="button">了解更多 <i class="fa fa-arrow-circle-right"></i> <!-- » --></a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
 
-
-      <!-- START THE FEATURETTES -->
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">软件开发<span class="text-muted">Development</span></h2>
-          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" tppabs="http://v3.bootcss.com/examples/carousel/holder.js/500x500/auto" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" tppabs="http://v3.bootcss.com/examples/carousel/holder.js/500x500/auto" alt="Generic placeholder image">
-        </div>
-        <div class="col-md-7">
-          <h2 class="featurette-heading">信息系统集成 <span class="text-muted">Infomation Collection</span></h2>
-          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">计算机系统集成 <span class="text-muted">System Collection</span></h2>
-          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" tppabs="http://v3.bootcss.com/examples/carousel/holder.js/500x500/auto" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <!-- /END THE FEATURETTES -->
-
+      <div class="row">
+        <div class="col-lg-4">
+        <span class="fa-stack fa-5x text-muted" aria-hidden="true">
+          <i class="fa fa-circle fa-stack-2x "></i>
+          <i class="fa fa-hashtag fa-stack-1x fa-inverse "></i>
+        </span>
+          <h2>软件开发</h2>
+          <!-- <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p> -->
+          <p><a class="btn btn-default" href="#" role="button">了解更多 <i class="fa fa-arrow-circle-right"></i> <!-- » --></a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+        <span class="fa-stack fa-5x text-info" aria-hidden="true">
+          <i class="fa fa-circle fa-stack-2x "></i>
+          <i class="fa fa-cubes fa-stack-1x fa-inverse "></i>
+        </span>
+          <h2>信息系统集成</h2>
+          <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
+          <p><a class="btn btn-default" href="#" role="button">了解更多 <i class="fa fa-arrow-circle-right"></i> <!-- » --></a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+        <span class="fa-stack fa-5x text-danger" aria-hidden="true">
+          <i class="fa fa-circle fa-stack-2x "></i>
+          <i class="fa fa-cogs fa-stack-1x fa-inverse "></i>
+        </span>
+          <h2>计算机系统集成</h2>
+          <!-- <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p> -->
+          <p><a class="btn btn-default" href="#" role="button">了解更多 <i class="fa fa-arrow-circle-right"></i> <!-- » --></a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
 
       <!-- FOOTER -->
       <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>© 2014 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+        <p class="pull-right"><a href="#">返回顶部</a></p>
+        <p>© 2016 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
       </footer>
 
     </div><!-- /.container -->
